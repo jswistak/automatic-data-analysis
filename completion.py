661a0060655191c93e06d930b5afe406df1c04d2
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-11-11 21:45:02 +0100

    Created interface for universal runtime (which handles report too)

--- completion.py
+++ completion.py
@@ -28,7 +28,7 @@ def get_response(messages: list[dict], system_message_suffix: str = None) -> dic
                 "content": system_message_suffix,
             }
         ]
-    print(messages_local)
+    # print(messages_local)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages_local,
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from os import getenv
 from conversation import Conversation
 from completion import ConversationRoles
 from utils import Colors, print_assistant_message, print_user_message
-from remote_python_shell_handler import RemotePythonShellHandler
+from runtime.ssh_python_runtime import SSHPythonRuntime
 from prompts import INITIAL_PROMPT, PROMPT_SUFFIX
+from datetime import datetime
 
 
 def main():
     load_dotenv()
-    python_runtime = RemotePythonShellHandler(
+    runtime = SSHPythonRuntime(
         host=getenv("SSH_HOST"),
         username=getenv("SSH_USERNAME"),
         password=getenv("SSH_PASSWORD"),
@@ -18,12 +19,13 @@ def main():
 
     # region: Loading dataset into runtime environment
     dataset_file_name = getenv("DATASET_PATH").split("/")[-1]
-    python_runtime.transfer_file(getenv("DATASET_PATH"), dataset_file_name)
+    runtime.upload_file(getenv("DATASET_PATH"), dataset_file_name)
 
     load_dataset_code = "\n".join(
         ["import pandas as pd", f"df= pd.read_csv('{dataset_file_name}', sep=',')"]
     )
-    python_runtime.execute(load_dataset_code)
+    cell_idx = runtime.add_code(load_dataset_code)
+    runtime.execute_cell(cell_idx)
     # endregion
 
     # Setting initial conversation goals
@@ -37,10 +39,12 @@ def main():
         python_code_executed=load_dataset_code,
     )
 
+    cell_idx = runtime.add_code("df.head()")
+    runtime.execute_cell(cell_idx)
     user_message: str = (
         "Here is a dataset I want you to analyze. "
         "It is a CSV file, loaded into pandas as a 'df' variable. Here is the output of the ```python\df.head()```\n"
-        f"{python_runtime.execute('df.head()')}"
+        f"{runtime.get_cell_output(cell_idx)}"
     )
 
     while "q" not in input(
@@ -80,11 +84,10 @@ def main():
             retval = None
             if code.startswith("python"):
                 code = code[6:]
-                try:
-                    bot.add_executed_code(code)
-                    retval = python_runtime.execute(code)
-                except Exception as e:
-                    retval = f"Error occurred while executing code: {e}"
+                bot.add_executed_code(code)
+                cell_idx = runtime.add_code(code)
+                runtime.execute_cell(cell_idx)
+                retval = runtime.get_cell_output(cell_idx)
             user_message = (
                 user_message
                 + "\n"
@@ -94,7 +97,7 @@ def main():
                     retval
                     if retval
                     else (
-                        "Code has been executed without any errors! Unfortunately, there is no output for this code snippet. Please remember to print the output."
+                        "Code has been executed! Unfortunately, there is no output for this code snippet. Please remember to print the output."
                         if ".show" not in code
                         else "While showing the plots it very helpful for me to understand the data. "
                         "Please also remember that I'm unable to provide you with the plots. Can you then also print the required numerical description for me to present it to you?"
@@ -109,6 +112,8 @@ def main():
 
     # Save conversation
     bot.save_conversation_to_file()
+    report_path = runtime.generate_report("reports", datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    print(f"Report has been saved to {report_path}")
 
 
 if __name__ == "__main__":
