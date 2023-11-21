aaf2407043487da17cdbdcfad460fbe5fa34fa78
Jakub Swistak <jakub.swistak@gmail.com>
2023-11-21 18:58:21 +0100

    conversation variables internal

--- conversation.py
+++ conversation.py
@@ -12,11 +12,11 @@ class Conversation:
     """Conversation class that handles the conversation flow and stores the conversation history."""
     def __init__(self, runtime: IRuntime, code_assistant: IAssistant, analysis_assistant: IAssistant,
                  prompt: IPromptManager, conversation: List[Message] = None):
-        self.conversation: List[Message] = conversation
-        self.runtime: IRuntime = runtime
-        self.code_assistant: IAssistant = code_assistant
-        self.analysis_assistant: IAssistant = analysis_assistant
-        self.prompt: IPromptManager = prompt
+        self._conversation: List[Message] = conversation
+        self._runtime: IRuntime = runtime
+        self._code_assistant: IAssistant = code_assistant
+        self._analysis_assistant: IAssistant = analysis_assistant
+        self._prompt: IPromptManager = prompt
 
     @staticmethod
     def format_code_assistant_message(message: str, code_output: str) -> str:
@@ -34,36 +34,36 @@ class Conversation:
 
     def get_conversation(self) -> List[Message]:
         """Get the conversation."""
-        return self.conversation
+        return self._conversation
 
     def _add_to_conversation(self, role: ConversationRolesInternalEnum, content: str) -> None:
-        self.conversation.append(Message(role=role, content=content))
+        self._conversation.append(Message(role=role, content=content))
 
     def _get_last_message(self) -> Message:
-        return self.conversation[-1]
+        return self._conversation[-1]
 
     def _send_message_analysis(self) -> None:
-        analysis_conv = self.prompt.generate_conversation_context(self.conversation,
-                                                                  ConversationRolesInternalEnum.ANALYSIS, LLMType.GPT4)
-        analysis_response = self.analysis_assistant.generate_response(analysis_conv)
+        analysis_conv = self._prompt.generate_conversation_context(self._conversation,
+                                                                   ConversationRolesInternalEnum.ANALYSIS, LLMType.GPT4)
+        analysis_response = self._analysis_assistant.generate_response(analysis_conv)
         self._add_to_conversation(ConversationRolesInternalEnum.ANALYSIS, analysis_response)
         print_message(self._get_last_message(), Colors.BLUE)
 
     def _execute_python_snippet(self, code: str) -> int:
-        cell_idx = self.runtime.add_code(code)
-        self.runtime.execute_cell(cell_idx)
+        cell_idx = self._runtime.add_code(code)
+        self._runtime.execute_cell(cell_idx)
         return cell_idx
 
     def _check_if_plot_in_output(self, cell_idx: int) -> bool:
-        return self.runtime.check_if_plot_in_output(cell_idx)
+        return self._runtime.check_if_plot_in_output(cell_idx)
 
     def _get_cell_output_stream(self, cell_idx: int) -> str:
-        return self.runtime.get_cell_output_stream(cell_idx)
+        return self._runtime.get_cell_output_stream(cell_idx)
 
     def _send_message_code(self) -> None:
-        code_conv = self.prompt.generate_conversation_context(self.conversation, ConversationRolesInternalEnum.CODE,
-                                                              LLMType.GPT4)
-        code_response = self.code_assistant.generate_response(code_conv)
+        code_conv = self._prompt.generate_conversation_context(self._conversation, ConversationRolesInternalEnum.CODE,
+                                                               LLMType.GPT4)
+        code_response = self._code_assistant.generate_response(code_conv)
         code_snippets = self._extract_code_snippets_from_message(code_response)
         output = []
         for code_snippet in code_snippets:
@@ -94,4 +94,4 @@ class Conversation:
 
     def get_conversation_json(self) -> str:
         """Get the conversation in json format."""
-        return json.dumps([message.model_dump_json() for message in self.conversation])
+        return json.dumps([message.model_dump_json() for message in self._conversation])
